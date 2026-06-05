5fa30daeeb54e1d7dfcdf8e24e6bf44c286dee0b
Julian Riedinger <julianriedinger27@gmail.com>
2026-06-05 11:36:20 -0300

    feat: se agrego migración para la tabla de categorias

---
+++ backend/migrations/categoria.migration.js
@@ -0,0 +1,28 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("categorias", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      nombre: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("categorias");
+  },
+};
