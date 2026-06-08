d704ee0ca3b884127c1a8ba35907dea6fbf8e60a
Julian Riedinger <julianriedinger27@gmail.com>
2026-06-08 19:16:34 -0300

    fix: se eliminaron timestamps en el seeder

--- backend/seeders/20260605145618-categorias.js
+++ backend/seeders/20260605145618-categorias.js
@@ -4,12 +4,12 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert("categorias", [
-      { nombre: "Ciencia Ficcion", createdAt: new Date(), updatedAt: new Date() },
-      { nombre: "Historia", createdAt: new Date(), updatedAt: new Date() },
-      { nombre: "Fantasia", createdAt: new Date(), updatedAt: new Date() },
-      { nombre: "Misterio", createdAt: new Date(), updatedAt: new Date() },
-      { nombre: "Romance", createdAt: new Date(), updatedAt: new Date() },
-      { nombre: "Terror", createdAt: new Date(), updatedAt: new Date() },
+      { nombre: "Ciencia Ficcion" },
+      { nombre: "Historia" },
+      { nombre: "Fantasia" },
+      { nombre: "Misterio" },
+      { nombre: "Romance" },
+      { nombre: "Terror" },
     ]);
   },
 
