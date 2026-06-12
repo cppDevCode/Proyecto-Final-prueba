8542e686c8b6cfb08b7b1405b56464bc02cfe7b6
Clara <vclarazivano@gmail.com>
2026-06-12 20:42:01 -0300

    feat: modelo de usuario, interfaz, controller y router

---
+++ backend/models/usuario.model.ts
@@ -0,0 +1,56 @@
+import { Table, Column, Model, PrimaryKey, AutoIncrement, AllowNull } from 'sequelize-typescript';
+import { DataType } from 'sequelize-typescript';
+import { InterfaceUsuario } from '../interfaces/Usuario-interface';
+
+@Table({
+    tableName: 'usuarios',
+    timestamps: true 
+})
+
+
+export class Usuario extends Model<InterfaceUsuario> implements InterfaceUsuario {
+    @PrimaryKey
+    @AutoIncrement
+    @Column(DataType.INTEGER)
+    id?: number;
+
+    @AllowNull(false)
+    @Column(DataType.STRING)
+    nombre!: string;
+
+    @AllowNull(false)
+    @Column(DataType.STRING)
+    contrasenia!: string;
+
+    @AllowNull(false)
+    @Column(DataType.STRING)
+    mail!: string;
+
+
+    static async encontrarPorId (id: number): Promise<InterfaceUsuario | null> {
+        return await Usuario.findByPk(id);
+    }
+
+    static async traerTodos (): Promise<InterfaceUsuario[] | []> {
+        return await Usuario.findAll();
+    }
+
+    static async crear(usuario: InterfaceUsuario): Promise<InterfaceUsuario> {
+        return await Usuario.create(usuario);
+    }
+
+
+    static async borrarPorId(id:number): Promise<InterfaceUsuario | null> {
+        const usuarioBd: Usuario | null = await Usuario.findByPk(id);
+        let salida: any = null;
+        if (usuarioBd) {
+            salida = usuarioBd;
+            await usuarioBd.destroy();
+        }
+        return salida;
+    }
+}
+
+
+
+
---
+++ backend/routes/usuarios-routes.ts
@@ -0,0 +1,27 @@
+import { UsuariosController } from "../controllers/usuarios-controller";
+import express, { Router } from 'express';
+
+export class UsuariosRouter {
+    private router: Router;
+    
+    public constructor() {
+        this.router = express.Router();
+        this.rutasUsuarios();
+    }
+
+    private rutasUsuarios():void {
+        const usuarioC:UsuariosController = new UsuariosController();
+        
+        this.router.get('/', usuarioC.getUsuarios);
+        //this.router.post('/', usuariosC.postUsuario); 
+       // this.router.put('/:id', usuariosC.putUsuario);
+
+    }
+
+    public getRouter(): Router {
+        return this.router;
+    }
+}
+
+
+
